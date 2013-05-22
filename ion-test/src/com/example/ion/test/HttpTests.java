2258ea1c7e157f983b719719cb6cecc5e8780b56
Koushik Dutta <koushd@gmail.com>
2013-05-22 00:47:11 -0700

    json test

--- ion-test/src/com/example/ion/test/HttpTests.java
+++ ion-test/src/com/example/ion/test/HttpTests.java
@@ -4,6 +4,7 @@ import android.test.AndroidTestCase;
 import com.koushikdutta.async.AsyncServer;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
+import org.json.JSONObject;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -13,12 +14,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class HttpTests extends AndroidTestCase {
     public void testString() throws Exception {
-        assertNotNull(Ion.with(getContext()).load("http://www.clockworkmod.com/").asString().get());
+        assertNotNull(Ion.with(getContext()).load("https://raw.github.com/koush/ion/master/ion-test/testdata/test.json").asString().get());
     }
 
     public void testStringWithCallback() throws Exception {
         final Semaphore semaphore = new Semaphore(0);
-        Ion.with(getContext()).load("http://www.clockworkmod.com/")
+        Ion.with(getContext())
+                .load("http://www.clockworkmod.com/")
                 // need to null out the handler since the semaphore blocks the main thread
                 .setHandler(null)
                 .asString()
@@ -31,5 +33,11 @@ public class HttpTests extends AndroidTestCase {
                     }
                 });
         assertTrue(semaphore.tryAcquire(1000000, TimeUnit.MILLISECONDS));
+    }
+
+    public void testJSONObject() throws Exception {
+        JSONObject ret;
+        assertNotNull(ret = Ion.with(getContext()).load("https://raw.github.com/koush/ion/master/ion-test/testdata/test.json").asJSONObject().get());
+        assertEquals("bar", ret.getString("foo"));
     }
 }
