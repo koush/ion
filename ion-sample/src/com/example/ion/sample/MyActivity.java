bfff243607c44cb126da480ab73473a3291f7e63
Koushik Dutta <koushd@gmail.com>
2013-05-30 11:51:46 -0700

    sample

---
+++ ion-sample/src/com/example/ion/sample/MyActivity.java
@@ -0,0 +1,15 @@
+package com.example.ion.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
