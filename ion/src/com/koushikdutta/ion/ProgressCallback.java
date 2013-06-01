7c0009fed938c8869c34ba1d1adefe8797de1e91
Koushik Dutta <koushd@gmail.com>
2013-06-01 00:16:10 -0700

    document ProgressCallback

--- ion/src/com/koushikdutta/ion/ProgressCallback.java
+++ ion/src/com/koushikdutta/ion/ProgressCallback.java
@@ -4,5 +4,10 @@ package com.koushikdutta.ion;
  * Callback that is invoked on download progress
  */
 public interface ProgressCallback {
+    /**
+     * onProgress is invoked periodically during a request download
+     * @param downloaded The number of bytes currently downloaded
+     * @param total The total number of bytes in this request, or -1 if unknown
+     */
     void onProgress(int downloaded, int total);
 }
