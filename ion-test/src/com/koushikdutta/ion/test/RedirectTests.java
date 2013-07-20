3b0cfa3e9c0c75f2372df412bfc9864c18841e7f
Koushik Dutta <koushd@gmail.com>
2013-07-20 10:35:48 -0700

    Add tests to verify final redirected URI is available with the Response.
    Change-Id: Ied1fff8c208e84a3f9e359743b486fe163963308

---
+++ ion-test/src/com/koushikdutta/ion/test/RedirectTests.java
@@ -0,0 +1,47 @@
+package com.koushikdutta.ion.test;
+
+import android.test.AndroidTestCase;
+
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
+import com.koushikdutta.async.http.server.AsyncHttpServerResponse;
+import com.koushikdutta.async.http.server.HttpServerRequestCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+/**
+ * Created by koush on 7/20/13.
+ */
+public class RedirectTests extends AndroidTestCase {
+    public void testFinalLocation() throws Exception {
+        try {
+            AsyncHttpServer server = new AsyncHttpServer();
+            server.listen(Ion.getDefault(getContext()).getServer(), 5000);
+            server.get("/", new HttpServerRequestCallback() {
+                @Override
+                public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                    response.redirect("/foo");
+                }
+            });
+
+            server.get("/foo", new HttpServerRequestCallback() {
+                @Override
+                public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                    response.send("bar");
+                }
+            });
+
+            Response<String> response = Ion.with(getContext())
+            .load("http://localhost:5000")
+            .asString()
+            .withResponse()
+            .get();
+
+            assertEquals(response.getResult(), "bar");
+            assertEquals(response.getRequest().getUri().toString(), "http://localhost:5000/foo");
+        }
+        finally {
+            Ion.getDefault(getContext()).getServer().stop();
+        }
+    }
+}
