ff4a09712f72209ea4a6a9ea4cd8965fd0d52f08
Wolfram Rittmeyer <wolfram.rittmeyer@grokkingandroid.com>
2013-11-19 00:20:03 +0100

    Added the possibility to add multiple params at once using a map.
    Added methods to the respective interfaces and changed IonRequestBuilder to implement those additional methods.

--- ion/src/com/koushikdutta/ion/builder/UrlEncodedBuilder.java
+++ ion/src/com/koushikdutta/ion/builder/UrlEncodedBuilder.java
@@ -1,5 +1,8 @@
 package com.koushikdutta.ion.builder;
 
+import java.util.List;
+import java.util.Map;
+
 /**
 * Created by koush on 5/30/13.
 */ // set additional body parameters for url form encoded
@@ -12,4 +15,11 @@ public interface UrlEncodedBuilder<U extends UrlEncodedBuilder> {
      * @return
      */
     public U setBodyParameter(String name, String value);
+    /**
+     * Specifies a map with application/x-www-form-urlencoded name and value pairs to send to the HTTP server.
+     * If no HTTP method was explicitly provided in the load call, the default HTTP method, POST, is used.
+     * @param params The map containing key value pairs
+     * @return
+     */
+    public U setBodyParameters(Map<String, List<String>> params);
 }
