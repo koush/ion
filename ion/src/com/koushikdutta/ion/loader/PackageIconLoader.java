6b8dd7af5949f42b16956d18eb7eae17bb8d2251
Koushik Dutta <koushd@gmail.com>
2013-11-27 21:11:11 -0800

    fix NPE

--- ion/src/com/koushikdutta/ion/loader/PackageIconLoader.java
+++ ion/src/com/koushikdutta/ion/loader/PackageIconLoader.java
@@ -35,7 +35,7 @@ public class PackageIconLoader implements Loader {
     @Override
     public Future<BitmapInfo> loadBitmap(final Ion ion, final String uri) {
         final URI request = URI.create(uri);
-        if (!request.getScheme().startsWith("package"))
+        if (request == null || request.getScheme() == null || !request.getScheme().startsWith("package"))
             return null;
 
         final SimpleFuture<BitmapInfo> ret = new SimpleFuture<BitmapInfo>();
