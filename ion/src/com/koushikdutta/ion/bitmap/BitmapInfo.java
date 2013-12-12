2069e4fa7a8f8fd7a9c76d46831cc8f9197c3fda
Koushik Dutta <koushd@gmail.com>
2013-12-11 18:37:03 -0800

    herp derp
    Change-Id: Ic7dc0d19ab6f58f3ab2d38fd08c364a57073d5c5

--- ion/src/com/koushikdutta/ion/bitmap/BitmapInfo.java
+++ ion/src/com/koushikdutta/ion/bitmap/BitmapInfo.java
@@ -1,8 +1,6 @@
 package com.koushikdutta.ion.bitmap;
 
-import android.annotation.TargetApi;
 import android.graphics.Bitmap;
-import android.os.Build;
 
 import java.lang.ref.WeakReference;
 
@@ -19,14 +17,9 @@ public class BitmapInfo {
     public Exception exception;
     public WeakReference<Bitmap> bitmapRef;
 
-    @TargetApi(19)
     public int sizeOf() {
         if (bitmaps == null)
             return 0;
-        if (Build.VERSION.SDK_INT == 19)
-            return bitmaps[0].getAllocationByteCount() * bitmaps.length;
-        else if (Build.VERSION.SDK_INT == 12)
-            return bitmaps[0].getByteCount() * bitmaps.length;
         return bitmaps[0].getRowBytes() * bitmaps[0].getHeight() * bitmaps.length;
     }
 }
