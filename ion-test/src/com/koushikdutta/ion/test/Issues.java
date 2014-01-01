0fa660f88a3a0d85034aa2cadae2ddf0735c0e4d
Koushik Dutta <koushd@gmail.com>
2014-01-01 15:49:45 -0800

    issue tests

--- ion-test/src/com/koushikdutta/ion/test/Issues.java
+++ ion-test/src/com/koushikdutta/ion/test/Issues.java
@@ -1,5 +1,6 @@
 package com.koushikdutta.ion.test;
 
+import android.graphics.Bitmap;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
@@ -8,8 +9,8 @@ import com.koushikdutta.ion.Ion;
 /**
  * Created by koush on 10/27/13.
  */
-public class Issue74 extends AndroidTestCase {
-    public void testIssue() throws Exception {
+public class Issues extends AndroidTestCase {
+    public void testIssue74() throws Exception {
         String data = Ion.with(getContext(), "https://raw.github.com/koush/AndroidAsync/master/AndroidAsyncTest/testdata/test.json")
         .setLogging("MyLogs", Log.VERBOSE)
         .asString().get();
@@ -19,5 +20,16 @@ public class Issue74 extends AndroidTestCase {
         .asString().get();
 
         assertEquals(data, data2);
+    }
+
+    public void testIssue126() throws Exception {
+        Bitmap bitmap = Ion.with(getContext())
+        .load("http://bdc.tsingyuan.cn/api/img?w=advanced")
+        .setLogging("Issue126", Log.VERBOSE)
+        .asBitmap()
+        .get();
+
+        assertNotNull(bitmap);
+        assertTrue(bitmap.getWidth() > 0);
     }
 }
