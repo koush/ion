78b8e332e15d048685261279ffd3627b801cf604
Koushik Dutta <koushd@gmail.com>
2014-01-30 10:27:20 -0800

    Missing file
    Change-Id: Id002a604d24b5dd1918a11c99ad9c7bd49259a4b

---
+++ ion/src/com/koushikdutta/ion/LoadBitmapRegion.java
@@ -0,0 +1,34 @@
+package com.koushikdutta.ion;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+import com.koushikdutta.ion.bitmap.BitmapInfo;
+
+/**
+ * Created by koush on 1/29/14.
+ */
+public class LoadBitmapRegion extends BitmapCallback {
+    public LoadBitmapRegion(final Ion ion, final String key, final BitmapRegionDecoder decoder, final Rect region, final int inSampleSize) {
+        super(ion, key, true);
+
+        Ion.getBitmapLoadExecutorService().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Bitmap bitmap = ion.bitmapCache.loadRegion(decoder, region, inSampleSize);
+                    if (bitmap == null)
+                        throw new Exception("failed to load bitmap region");
+                    BitmapInfo info = new BitmapInfo(new Bitmap[] { bitmap }, new Point(bitmap.getWidth(), bitmap.getHeight()));
+                    info.key = key;
+                    report(null, info);
+                }
+                catch (Exception e) {
+                    report(e, null);
+                }
+            }
+        });
+    }
+}
