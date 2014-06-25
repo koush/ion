3d77a10896d58335a07e73f35eb26f7229e378dd
Yoav Sternberg <yoav.goop@gmail.com>
2014-06-25 12:26:20 +0300

    Add support for 2.2
    copied copyOfRange from aosp

--- ion/src/com/koushikdutta/ion/gif/GifDecoder.java
+++ ion/src/com/koushikdutta/ion/gif/GifDecoder.java
@@ -183,7 +183,7 @@ public class GifDecoder extends Thread{
                 }
             }
 			if (lastPixels != null) {
-                dest = Arrays.copyOf(lastPixels, lastPixels.length);
+                dest = copyOfRange(lastPixels, 0, lastPixels.length);
                 // copy pixels
 				if (lastDispose == 2) {
 					// fill last image rect area with background color
@@ -704,4 +704,33 @@ public class GifDecoder extends Thread{
 			readBlock();
 		} while ((blockSize > 0) && !err());
 	}
+
+	/**
+     * Copies elements from {@code original} into a new array, from indexes start (inclusive) to
+     * end (exclusive). The original order of elements is preserved.
+     * If {@code end} is greater than {@code original.length}, the result is padded
+     * with the value {@code 0}.
+     *
+     * @param original the original array
+     * @param start the start index, inclusive
+     * @param end the end index, exclusive
+     * @return the new array
+     * @throws ArrayIndexOutOfBoundsException if {@code start < 0 || start > original.length}
+     * @throws IllegalArgumentException if {@code start > end}
+     * @throws NullPointerException if {@code original == null}
+     */
+    public static int[] copyOfRange(int[] original, int start, int end) {
+        if (start > end) {
+            throw new IllegalArgumentException();
+        }
+        int originalLength = original.length;
+        if (start < 0 || start > originalLength) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        int resultLength = end - start;
+        int copyLength = Math.min(resultLength, originalLength - start);
+        int[] result = new int[resultLength];
+        System.arraycopy(original, start, result, 0, copyLength);
+        return result;
+    }
 }
