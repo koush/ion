e7434107a059b6538c11aad74b19c60aa316cdd7
Koushik Dutta <koushd@gmail.com>
2014-10-27 16:34:22 -0700

    Make conscrypt the last provider, and not the default SSLContext
    Conflicts:
    	ion/src/com/koushikdutta/ion/conscrypt/ConscryptMiddleware.java

--- ion/src/com/koushikdutta/ion/conscrypt/ConscryptMiddleware.java
+++ ion/src/com/koushikdutta/ion/conscrypt/ConscryptMiddleware.java
@@ -7,6 +7,9 @@ import com.koushikdutta.async.future.Cancellable;
 import com.koushikdutta.async.http.AsyncSSLSocketMiddleware;
 import com.koushikdutta.async.http.SimpleMiddleware;
 
+import java.security.Provider;
+import java.security.Security;
+
 import javax.net.ssl.SSLContext;
 
 /**
@@ -33,9 +36,23 @@ public class ConscryptMiddleware extends SimpleMiddleware {
                 if (initialized)
                     return;
                 initialized = true;
+                SSLContext originalDefault = SSLContext.getDefault();
                 Context gms = context.createPackageContext("com.google.android.gms", Context.CONTEXT_INCLUDE_CODE | Context.CONTEXT_IGNORE_SECURITY);
-                Class clazz = gms.getClassLoader().loadClass("com.google.android.gms.common.security.ProviderInstallerImpl");
-                clazz.getMethod("insertProvider", Context.class).invoke(null, context);
+                gms
+                .getClassLoader()
+                .loadClass("com.google.android.gms.common.security.ProviderInstallerImpl")
+                .getMethod("insertProvider", Context.class)
+                .invoke(null, context);
+
+                Provider[] providers = Security.getProviders();
+                for (Provider provider: providers) {
+                    if (GMS_PROVIDER.equals(provider.getName())) {
+                        Security.removeProvider(GMS_PROVIDER);
+                        Security.insertProviderAt(provider, providers.length);
+                        SSLContext.setDefault(originalDefault);
+                        break;
+                    }
+                }
                 success = true;
             }
         }
@@ -43,12 +60,21 @@ public class ConscryptMiddleware extends SimpleMiddleware {
         }
     }
 
+    private static final String GMS_PROVIDER = "GmsCore_OpenSSL";
+
     public void initialize() {
         initialize(context);
         if (success && !instanceInitialized && enabled) {
             instanceInitialized = true;
             try {
-                SSLContext sslContext = SSLContext.getInstance("TLS");
+                SSLContext sslContext = null;
+                try {
+                    sslContext = SSLContext.getInstance("TLS", GMS_PROVIDER);
+                }
+                catch (Exception e) {
+                }
+                if (sslContext == null)
+                    sslContext = SSLContext.getInstance("TLS");
                 sslContext.init(null, null, null);
                 if (middleware.getSSLContext() != AsyncSSLSocketWrapper.getDefaultSSLContext())
                     middleware.setSSLContext(sslContext);
