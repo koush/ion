8af49b74dc22d7eee2fa5d1f8168ecab6f11e3b3
Koushik Dutta <koushd@gmail.com>
2019-03-15 17:21:36 -0700

    update

--- ion-kotlin/src/com/koushikdutta/ion/kotlin/AsyncAwait.kt
+++ ion-kotlin/src/com/koushikdutta/ion/kotlin/AsyncAwait.kt
@@ -12,15 +12,11 @@ import com.koushikdutta.async.future.SimpleFuture
 import com.koushikdutta.ion.ContextReference
 import com.koushikdutta.ion.IonContext
 import java.util.concurrent.Executor
-import kotlin.coroutines.experimental.*
+import kotlin.coroutines.*
 
 /**
  * Created by koush on 7/2/17.
  */
-
-private class WrappingContextReference {
-
-}
 
 fun <T> Activity.async(block: suspend AsyncContext.() -> T): Future<T> {
     return async(ContextReference.ActivityContextReference(this), block)
@@ -38,7 +34,7 @@ fun <T> android.support.v4.app.Fragment.async(block: suspend AsyncContext.() -> 
     return async(ContextReference.SupportFragmentContextReference(this), block)
 }
 
-class AsyncContext(val ionContext: IonContext, val future: SimpleFuture<Object>): IonContext {
+class AsyncContext(private val ionContext: IonContext, private val future: SimpleFuture<*>) : IonContext {
     override fun getContext(): Context? {
         return ionContext.context
     }
@@ -103,20 +99,19 @@ class AsyncContext(val ionContext: IonContext, val future: SimpleFuture<Object>)
     }
 }
 
-private class StandaloneCoroutine<T>(val asyncContext: AsyncContext, val future: SimpleFuture<T>, override val context: CoroutineContext): Continuation<T> {
-    override fun resume(value: T) {
-        if (asyncContext.isAlive() == null)
-            future.setComplete(value)
-        else
-            future.cancelSilently()
-    }
-
-    override fun resumeWithException(exception: Throwable) {
-        if (asyncContext.isAlive() == null) {
-            if (exception is Exception)
-                future.setComplete(exception)
-            else
-                future.setComplete(Exception(exception))
+private class StandaloneCoroutine<T>(val asyncContext: AsyncContext, val future: SimpleFuture<T>, override val context: CoroutineContext) : Continuation<T> {
+    override fun resumeWith(result: Result<T>) {
+        if (asyncContext.isAlive == null) {
+            if (result.isSuccess) {
+                future.setComplete(result.getOrNull())
+            }
+            else {
+                val exception = result.exceptionOrNull();
+                if (exception is Exception)
+                    future.setComplete(exception)
+                else
+                    future.setComplete(Exception(exception))
+            }
         }
         else {
             future.cancelSilently()
@@ -126,8 +121,8 @@ private class StandaloneCoroutine<T>(val asyncContext: AsyncContext, val future:
 
 fun <T> async(ionContext: IonContext, block: suspend AsyncContext.() -> T): Future<T> {
     val future = SimpleFuture<T>()
-    val asyncContext = AsyncContext(ionContext, future as SimpleFuture<Object>)
-    val coroutineContext = StandaloneCoroutine<T>(asyncContext, future, EmptyCoroutineContext)
+    val asyncContext = AsyncContext(ionContext, future)
+    val coroutineContext = StandaloneCoroutine(asyncContext, future, EmptyCoroutineContext)
     block.startCoroutine(asyncContext, coroutineContext)
     return future
 }
